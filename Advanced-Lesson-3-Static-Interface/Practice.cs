185e7ed983462d306cbc3f963423f03a49e4270e
Mihail83 <Sklif83@tut.by>
2019-01-28 00:42:44 +0300

    AL3-P3/3. GuessType.  END

--- Advanced-Lesson-3-Static-Interface/Practice.cs
+++ Advanced-Lesson-3-Static-Interface/Practice.cs
@@ -41,6 +41,16 @@ namespace Advanced_Lesson_3_Static_Interface
         /// </summary>
         public static void AL3_P3_3()
         {
+            //string test = "Hello";
+            //int test = -15;
+            //Double test = 1.1591;
+            //decimal test = 1.01591m;
+            //float test = 4.286f;
+             DateTime test = DateTime.Now;
+            // UniqueItem test = new UniqueItem();
+
+            var guessType = new GuessTypeClass();
+            guessType.GuessType(test);
         }
 
     }
@@ -66,7 +76,62 @@ namespace Advanced_Lesson_3_Static_Interface
         }
 
         public void SetId(int number) => _counter = number;
-
     }
 
+    // AL3-P3/3.////////////////////////////////////////////////////////////////////////////////
+    public class GuessTypeClass
+    {
+        public void GuessType<T>(T item)
+        {
+            Type type = item.GetType();
+           
+            switch (type.Name)
+            {
+                case  "String" :                     
+                    Console.WriteLine($" Тип:  {type.Name},  количество символов  -  {(item as String).Length}");
+                    break;
+
+                case "Int32":
+                    Int32 number = Convert.ToInt32(item);
+                    string answer;
+                    if (number < 0)
+                    {
+                        answer = "отрицательное";
+                    }
+                    else
+                    {
+                        answer = "положительное";
+                    }
+                    Console.Write($" Тип:  {type.Name},  {answer} "); 
+                    break;
+
+                case "Double":
+                case "Single":
+                case "Decimal":
+                    var floatPoint = Convert.ToDecimal(item);                              
+                    while (floatPoint - (long)floatPoint !=0)
+                    {
+                        floatPoint *= 10;                        
+                    }
+                    long floatlong = (long)floatPoint;
+                    int counter = 0;
+                    while (floatlong>0)
+                    {
+                        floatlong /= 10;
+                        counter++;
+                    }
+                                    //   ((int)Math.Log10(floatTemp) + 1);
+                                    //   floatPoint.ToString().Length;
+                    Console.WriteLine($"{type.Name},   количество значимых цифр  -  {counter}");
+                    break;
+                case "DateTime":
+                    Console.WriteLine($"{type.Name} -  {Convert.ToDateTime(item)}   это дата и время  ");
+                    break;
+
+                default:
+                    Console.WriteLine("непонятная хрень");
+                    break;
+            }
+        }
+    }
 }
